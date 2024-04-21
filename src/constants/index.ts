fb1e084bde457c5f587ccdd8c41143a624b1a0a6
Ale dev <pimentelaleee@gmail.com>
2024-04-21 18:29:46 -0300

    start project

---
+++ src/constants/index.ts
@@ -0,0 +1,31 @@
+export const sections = [
+    {
+        id: "about-me",
+        title: "About Me",
+    },
+    {
+        id: "experience",
+        title: "Experience",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+
+export const workExperience = [
+    {
+        title: 'Ventia | Full Stack Developer',
+        description: `Working as a full stack developer and encharge of the developments for clients. Working with technologies like React, Node Js, Express, MongoDB, and Next Js.`,
+        date: 'Current...'
+    }, {
+        title: 'Tata Consultancy Services | Trainee - Junior Salesforce Developer',
+        description: `Started my journey as a Salesforce Developer, learning the basics of the platform and the development of custom solutionsfor the company's clients.Worked for Jhonson & Jhonson.`,
+        date: 'September 2022'
+    }, {
+        title: 'E-Trax | Frontend Developer',
+        description: `Encharge of developing the front-end of the company's usingReact Native, creating graphic designs and also developing emails templates for the company's marketing campaigns.`,
+        date: 'August 2021'
+    }
+]
