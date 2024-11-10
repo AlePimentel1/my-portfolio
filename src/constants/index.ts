68ddd8159c807810e535000543c0751803115c90
Ale dev <pimentelaleee@gmail.com>
2024-11-10 17:09:54 -0300

    new ui for my portfolio

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,16 +16,19 @@ export const sections = [
 
 export const workExperience = [
     {
-        title: 'Ventia | Full Stack Developer',
+        title: 'Ventia',
         description: `Working as a full stack developer and encharge of the developments for clients. Working with technologies like React, Node Js, Express, MongoDB, and Next Js.`,
-        date: 'Current...'
+        date: 'Current',
+        position: 'Full Stack Developer'
     }, {
-        title: 'Tata Consultancy Services | Trainee - Junior Salesforce Developer',
+        title: 'Tata Consultancy Services',
         description: `Started my journey as a Salesforce Developer, learning the basics of the platform and the development of custom solutionsfor the company's clients.Worked for Jhonson & Jhonson.`,
-        date: 'September 2022'
+        date: 'September 2022',
+        position: 'Trainee - Junior Salesforce Developer'
     }, {
-        title: 'E-Trax | Frontend Developer',
-        description: `Encharge of developing the front-end of the company's usingReact Native, creating graphic designs and also developing emails templates for the company's marketing campaigns.`,
-        date: 'August 2021'
+        title: 'E-Trax',
+        description: `Encharge of developing the front-end of the company's using React Native, creating graphic designs and also developing emails templates for the company's marketing campaigns.`,
+        date: 'August 2021',
+        position: 'Frontend Developer'
     }
 ]
