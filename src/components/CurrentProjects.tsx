83f03d71cf7486ce52719e456591a0102a0584b4
Ale dev <pimentelaleee@gmail.com>
2025-01-03 13:31:53 -0300

    fix:

--- src/components/CurrentProjects.tsx
+++ src/components/CurrentProjects.tsx
@@ -4,12 +4,12 @@ const CurrentProjects = () => {
     return (
         <section className="py-24" id="projects">
             <div className="flex flex-col gap-6 px-4 mx-auto">
-                <h2 className="text-6xl md:text-8xl xl:text-4xl text-center font-bold font-heading font-heading tracking-px-n leading-none text-white">Current Projects</h2>
+                <h2 className="text-2xl md:text-3xl xl:text-4xl text-center font-bold font-heading font-heading tracking-px-n leading-none text-white">Current Projects</h2>
                 <p className="text-md text-pretty text-center font-medium leading-normal md:max-w-lg mx-auto text-white">Real projects that are currently in progress or active.</p>
                 <div className="mx-auto flex items-center justify-center">
-                    <div className="flex-wrap grid grid-cols-3 max-w-[80%]">
+                    <div className="flex-wrap grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 max-w-[80%]">
                         {projects.map((project, index) => (
-                            <div className="w-full col-span-1 flex" key={index}>
+                            <div className="w-full col-span-1 flex items-center justify-center" key={index}>
                                 <div className="p-2 h-full flex flex-col items-center gap-2 max-w-[80%] bg-black/90 border-2 rounded-xl [box-shadow:rgb(20,83,45)12px_12px] hover:[box-shadow:rgb(20,83,45)0px_0px] transition-all duration-300">
                                     <img className="object-contain w-auto h-48 rounded-xl" src={project.image} alt={project.title} />
                                     <div className="flex flex-col gap-2 justify-between h-full">
--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -17,7 +17,7 @@ export const Timeline: React.FC<TimelineProps> = ({ experiences }) => {
     return (
         <section className="py-24" id="experience">
             <div className="flex flex-col gap-6 px-4 mx-auto justify-center items-center">
-                <h2 className="text-6xl md:text-8xl xl:text-4xl text-center font-bold font-heading font-heading tracking-px-n leading-none text-white">Experience</h2>
+                <h2 className="text-2xl md:text-3xl xl:text-4xl text-center font-bold font-heading font-heading tracking-px-n leading-none text-white">Experience</h2>
                 <p className="text-md text-pretty text-center font-medium leading-normal md:max-w-lg mx-auto text-white">My professional journey so far.</p>
                 <div className="relative max-w-[80%]">
                     <div className="absolute left-1/2 top-0 h-full w-0.5 -translate-x-1/2 bg-green-800" />
